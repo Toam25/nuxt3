2f7a681f467fefb53e0451f35a26b4b830ccc678
Rado softiBox <r.rado@softibox.com>
2024-02-01 13:42:32 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <div>
+         <h1>RAKOTOARIMANANA</h1>
+         <h2>Rado Nirina</h2>
+      </div>
+      <div>
+          dev fullStack Php
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style>
+   body{
+    background-color: #fff4f4;
+   }
+</style>
